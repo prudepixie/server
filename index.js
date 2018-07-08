4fcf10d0601d9e903073b7d8bc9abe8513684370
Wendy Hsu <wndyhsu@gmail.com>
2018-07-08 15:05:20 -0700

    add passport for oauth

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // common js modules
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 // single page application
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
     res.send({ hello: 'wendy'});
