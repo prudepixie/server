3a0e65e0c653a0ce5af11e057b079f0a921f7d5f
Wendy Hsu <wndyhsu@gmail.com>
2018-07-09 15:01:36 -0700

    missed /in url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: 'auth/google/callback'
+    callbackURL: '/auth/google/callback'
     }, (accessToken) => {
         console.log('token', accessToken)        
     })
@@ -22,6 +22,11 @@ app.get(
         scope: ['profile', 'email']
     })
 );
+
+app.get(
+    '/auth/google/callback', 
+    passport.authenticate('google')
+)
 
 const PORT = process.env.PORT || 5000;
 
