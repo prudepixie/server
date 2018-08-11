bf685d52f3e6e0f7b29ce5bb5345f6ec0c707772
Wendy Hsu <wndyhsu@gmail.com>
2018-08-10 18:00:57 -0700

    add serialize/ deserialize user

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const keys = require('./config/keys');
 
 require('./models/User');
 require('./services/passport');
-mongoose.connection.on('connected', () => console.log('Connected'));
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 // single page application
 const app = express();
 require('./routes/authRoutes')(app);
