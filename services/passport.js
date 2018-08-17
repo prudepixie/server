da77deed6a25c31c93bc23238b503ecc46532734
Wendy Hsu <wndyhsu@gmail.com>
2018-08-16 19:36:54 -0700

    refactor promise function

--- services/passport.js
+++ services/passport.js
@@ -21,17 +21,14 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
         proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id} )
-            .then((existingUser) => {
-                if (existingUser) {
-                    done(null, existingUser);
-                } else {
-                    new User({ googleId: profile.id }).save()
-                    .then((user) => {
-                        done(null, user);
-                    })
-                }
-            })
+    }, 
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id} )
+        if (existingUser) {
+            done(null, existingUser);
+        } else {
+            const user = await new User({ googleId: profile.id }).save()
+            done(null, user);
+        }
     })
 );
