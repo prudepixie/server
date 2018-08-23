d5bff14d57f3b46e10b3bc2d5465ad837995f454
Wendy Hsu <wndyhsu@gmail.com>
2018-08-22 17:20:20 -0700

    adding reducers file

--- services/passport.js
+++ services/passport.js
@@ -23,12 +23,12 @@ passport.use(
         proxy: true
     }, 
     async (accessToken, refreshToken, profile, done) => {
-        const existingUser = await User.findOne({ googleId: profile.id} )
+        const existingUser = await User.findOne({ googleId: profile.id});
         if (existingUser) {
-            done(null, existingUser);
-        } else {
-            const user = await new User({ googleId: profile.id }).save()
-            done(null, user);
-        }
+            return done(null, existingUser);
+        } 
+        
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
     })
 );
